8b3787f9744fd5ae45c5025e89a0582a239e26fe
Software-Today <nicoli.dev.430@gmail.com>
2022-09-22 21:43:27 -0700

    Init

---
+++ client/src/layout/dashboard/TopNavbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-absolute"
+       :class="{'bg-white': showMenu, 'navbar-transparent': !showMenu}">
+    <div class="container-fluid">
+      <div class="navbar-wrapper">
+        <div class="navbar-toggle d-inline" :class="{toggled: $sidebar.showSidebar}">
+          <button type="button"
+                  class="navbar-toggler"
+                  aria-label="Navbar toggle button"
+                  @click="toggleSidebar">
+            <span class="navbar-toggler-bar bar1"></span>
+            <span class="navbar-toggler-bar bar2"></span>
+            <span class="navbar-toggler-bar bar3"></span>
+          </button>
+        </div>
+        <!-- <a class="navbar-brand" href="#"></a> -->
+      </div>
+
+      <collapse-transition>
+        <div class="collapse navbar-collapse show" v-show="showMenu">
+          <ul class="navbar-nav" :class="$rtl.isRTL ? 'mr-auto' : 'ml-auto'">
+
+          </ul>
+        </div>
+      </collapse-transition>
+    </div>
+  </nav>
+</template>
+<script>
+  import { CollapseTransition } from 'vue2-transitions';
+  import Modal from '@/components/Modal';
+
+  export default {
+    components: {
+      CollapseTransition,
+      Modal
+    },
+    computed: {
+      routeName() {
+        const { name } = this.$route;
+        return this.capitalizeFirstLetter(name);
+      },
+      isRTL() {
+        return this.$rtl.isRTL;
+      }
+    },
+    data() {
+      return {
+        activeNotifications: false,
+        showMenu: false,
+        searchModalVisible: false,
+        searchQuery: ''
+      };
+    },
+    methods: {
+      capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      },
+      toggleNotificationDropDown() {
+        this.activeNotifications = !this.activeNotifications;
+      },
+      closeDropDown() {
+        this.activeNotifications = false;
+      },
+      toggleSidebar() {
+        this.$sidebar.displaySidebar(!this.$sidebar.showSidebar);
+      },
+      hideSidebar() {
+        this.$sidebar.displaySidebar(false);
+      },
+      toggleMenu() {
+        this.showMenu = !this.showMenu;
+      }
+    }
+  };
+</script>
+<style>
+</style>
---
+++ client/src/pages/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="row">
+      <card>
+        <base-alert type="info">
+          <span>This is a plain notification</span>
+        </base-alert>
+      </card>
+
+    <div class="col-md-12">
+      <card>
+        <div class="places-buttons">
+          <div class="row">
+            <div class="col-md-6 ml-auto mr-auto text-center">
+              <h4 class="card-title">
+                Notifications Places
+                <p class="category">Click to view notifications</p>
+              </h4>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-8 ml-auto mr-auto">
+              <div class="row">
+                <div class="col-md-4">
+                  <base-button type="primary" block @click="notifyVue('top', 'center')">Top Center</base-button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </card>
+    </div>
+  </div>
+</template>
+<script>
+  import NotificationTemplate from './Notifications/NotificationTemplate';
+  import { BaseAlert } from '@/components';
+
+  export default {
+    components: {
+      BaseAlert
+    },
+    data() {
+      return {
+        type: ["", "info", "success", "warning", "danger"],
+        notifications: {
+          topCenter: false
+        }
+      };
+    },
+    methods: {
+      notifyVue(verticalAlign, horizontalAlign) {
+        const color = Math.floor(Math.random() * 4 + 1);
+        this.$notify({
+          component: NotificationTemplate,
+          icon: "tim-icons icon-bell-55",
+          horizontalAlign: horizontalAlign,
+          verticalAlign: verticalAlign,
+          type: this.type[color],
+          timeout: 0
+        });
+      }
+    }
+  };
+</script>
+<style>
+</style>
